ab3ede8b73d7f9813629b577babf657166b4df7a
Michal Huzevka <michalhuzevka@Michals-MacBook-Pro.local>
2015-08-27 18:21:17 +1000

    updated firebase config

--- client/src/js/config/config.js
+++ client/src/js/config/config.js
@@ -1,9 +1,9 @@
 var Config = {};
 
 if (process.env.NODE_ENV === 'production') {
-  Config.firebaseUrl = 'https://watch-edition-prod.firebaseIO.com';
+  Config.firebaseUrl = 'https://watcheditiontest.firebaseio.com';
 } else {
-  Config.firebaseUrl = 'https://watch-edition-dev.firebaseIO.com';
+  Config.firebaseUrl = 'https://watcheditiontest.firebaseio.com';
 }
 
 module.exports = Config;
