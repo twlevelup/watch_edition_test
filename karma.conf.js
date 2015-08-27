66fe07f1022b7ddbee38c2a3d7f1552d58f16dea
Michal Huzevka <michalhuzevka@Michals-MacBook-Pro.local>
2015-08-27 18:01:19 +1000

    removed code coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
 
         // test results reporter to use
         // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['spec', 'coverage', 'threshold'],
+        reporters: ['spec', 'threshold'],
 
         coverageReporter: {
           'type' : 'text',
